bf6a3677b97fef1cebdf587ff9f01e269f132933
Hari06om <hariohmverma96@gmail.com>
2025-12-26 19:48:47 +0530

    add the file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [jokes, setJokes] = useState([])
 
   return (
     <>
     <h1>
       Chai and fullstack development!
     </h1>
+    <p>
+      Jokes count: {jokes.length}
+    </p>
+    {
+      jokes.map((joke, index) => (
+        <div key={joke.id }>
+          <h2>{joke.title}</h2>
+          <p>{joke.content}</p>
+        </div>
+      ))
+      }
     </>
   )
 }
