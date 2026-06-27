9c3faa9569f0b41d7acbb14a14b23d163b417960
Hari Om Verma <hariohmverma96@gmail.com>
2026-06-27 20:35:16 +0530

    basic learning

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-import  express from 'express';
-
-const app = express();
-
-const PORT = 3000;  
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT} 
-        http://localhost:${PORT}`);
-});
-
+console.log("Hari om Verma");
